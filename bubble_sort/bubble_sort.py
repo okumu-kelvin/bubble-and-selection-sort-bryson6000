4f0a9ea0e514c9623f70c7ecd797e0569cb52596
bryson6001 <bryson.onyimbo@strathmore.edu>
2025-06-20 12:10:18 +0300

    bubble sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -3,5 +3,5 @@ def bubble_sort(arr):
  for i in range(n):
 for j in range(n - i - 1)
  if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+         arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
