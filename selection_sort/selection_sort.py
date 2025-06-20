a58676e182fd527efbdb367be180bd3315840e49
bryson6001 <bryson.onyimbo@strathmore.edu>
2025-06-20 12:18:46 +0300

    select sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -4,7 +4,7 @@ def selection_sort(arr):
    min_index = i
   for j in range(i + 1, n):
     if arr[j] < arr[min_index]:
-    min_index = j
+   min_index = j
 
   arr[i], arr[min_index] = arr[min_index], arr[i]
   return arr
